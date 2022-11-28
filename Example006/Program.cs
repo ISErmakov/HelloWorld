5cbade885556a5309d79df9373dbccc97913e712
Ermakovis <ermakov.ugra@gmail.com>
2022-11-28 11:25:17 +0500

    add examples 5, 6, 7

---
+++ Example006/Program.cs
@@ -0,0 +1,19 @@
+﻿int a = new Random().Next(0, 10), 
+    b = new Random().Next(0, 10),
+    c = new Random().Next(0, 10),
+    d = new Random().Next(0, 10),
+    e = new Random().Next(0, 10);
+
+int max = a;
+
+if (max < b) max = b;
+if (max < c) max = c;
+if (max < d) max = d;
+if (max < e) max = e;
+
+Console.WriteLine(a);
+Console.WriteLine(b);
+Console.WriteLine(c);
+Console.WriteLine(d);
+Console.WriteLine(e);
+Console.WriteLine(max);
---
+++ Example007/Program.cs
@@ -0,0 +1,38 @@
+﻿Console.Clear();
+int xa = 20, ya = 0;
+int xb = 40, yb = 20;
+int xc = 0, yc = 20;
+
+Console.SetCursorPosition(xa,ya);
+Console.WriteLine('*');
+Console.SetCursorPosition(xb,yb);
+Console.WriteLine('*');
+Console.SetCursorPosition(xc,yc);
+Console.WriteLine('*');
+
+int count = 0;
+int x = xa,
+    y = ya;
+while (count < 10000) 
+{
+    int what = new Random().Next(0,3);
+    if (what == 0)
+    {
+        x = (x + xa) / 2;
+        y = (y + ya) / 2;
+    }
+    if (what == 1)
+    {
+        x = (x + xb) / 2;
+        y = (y + yb) / 2;
+    }
+    if (what == 2)
+    {
+        x = (x + xc)/2;
+        y = (y + yc)/2;
+    }
+    Console.SetCursorPosition(x, y);
+    Console.WriteLine('*');
+    count++;
+}
+Console.SetCursorPosition(0, 21);
